5ec54e1cdf245185cf65dc891c12b2e5aafebf56
Brian Kessler <kessler.bm@gmail.com>
2017-12-07 14:48:46 +0100

    Moved promise method to static resource to facilitate reuse

--- src/aura/ZV_Board/ZV_BoardHelper.js
+++ src/aura/ZV_Board/ZV_BoardHelper.js
@@ -1,38 +1,13 @@
 ({
 	loadBoardWithCells : function(component) {
-		var serverMethod = {
-			name: 'getBoard',
-			params: {
-				boardId: component.get('v.recordId')
-			}
-		};
-		return this.apexResolve(component, serverMethod);
-	},
-	
-	apexResolve: function(component, serverMethod) {
-		var action = component.get('c.' + serverMethod.name);
-		action.setParams(serverMethod.params);
-
-		return new Promise(function(resolve, reject) {
-			action.setCallback(this, function(response) {
-				if (response.getState() === 'SUCCESS') {
-					resolve(response.getReturnValue())
-				}
-				else {
-					var errors = response.getError();
-					var errorMessage = (errors && errors[0] && errors[0].message)
-						? 'Error message: ' + errors[0].message
-						: 'Unknown error';
-					reject (errorMessage);
-				}
-			}); 
-			$A.enqueueAction(action);
-		}); 
+		return $LCH.ServerApexPromise(component, 'getBoard', {
+			boardId: component.get('v.recordId')
+		});
 	},
 	
 	displayCells : function(component) {
 		return function(returnValue) {
-			console.log('######### displayCells returnValue', returnValue);
+			console.log('######### displayCells returnValue2', returnValue);
 		}
 	}
 })
