0cbe9ca9c3a432021c6a1af2c7e3886c64aa1025
Brian Kessler <kessler.bm@gmail.com>
2017-12-07 14:35:00 +0100

    code cleanup

--- src/aura/ZV_Board/ZV_BoardHelper.js
+++ src/aura/ZV_Board/ZV_BoardHelper.js
@@ -1,38 +1,33 @@
 ({
 	loadBoardWithCells : function(component) {
-		var params = {
-			serverMethod: 'getBoard',
-			serverParams: {
+		var serverMethod = {
+			name: 'getBoard',
+			params: {
 				boardId: component.get('v.recordId')
 			}
 		};
-		return this.apexResolve(component, params);
+		return this.apexResolve(component, serverMethod);
 	},
 	
-	apexResolve: function(component, params) {
-			console.log('####### apexResolve component', component);
-			console.log('####### apexResolve params', params);
-			var action = component.get('c.' + params.serverMethod);
-			action.setParams(params.serverParams);
-			console.log('####### apexResolve action', action);
-			return new Promise(function(resolve, reject) {
-			console.log('####### apexResolve Promise resolve', resolve);
-			console.log('####### apexResolve Promise reject', reject);
-				action.setCallback(this, function(response) {
-			console.log('####### apexResolve Promise setCallback response', response);
-					if (response.getState() === 'SUCCESS') {
-						resolve(response.getReturnValue())
-					}
-					else {
-						var errors = response.getError();
-						var errorMessage = (errors && errors[0] && errors[0].message)
-							? 'Error message: ' + errors[0].message
-							: 'Unknown error';
-						reject (errorMessage);
-					}
-				}); 
-				$A.enqueueAction(action);
+	apexResolve: function(component, serverMethod) {
+		var action = component.get('c.' + serverMethod.name);
+		action.setParams(serverMethod.params);
+
+		return new Promise(function(resolve, reject) {
+			action.setCallback(this, function(response) {
+				if (response.getState() === 'SUCCESS') {
+					resolve(response.getReturnValue())
+				}
+				else {
+					var errors = response.getError();
+					var errorMessage = (errors && errors[0] && errors[0].message)
+						? 'Error message: ' + errors[0].message
+						: 'Unknown error';
+					reject (errorMessage);
+				}
 			}); 
+			$A.enqueueAction(action);
+		}); 
 	},
 	
 	displayCells : function(component) {
