87f0e5316c25c00c7bf98c88274ebcc2fd9ef4ee
Brian Kessler <kessler.bm@gmail.com>
2017-12-07 15:10:01 +0100

    Populated cell row list

--- src/aura/ZV_Board/ZV_BoardHelper.js
+++ src/aura/ZV_Board/ZV_BoardHelper.js
@@ -6,8 +6,19 @@
 	},
 	
 	displayCells : function(component) {
-		return function(returnValue) {
-			console.log('######### displayCells returnValue2', returnValue);
+		return function(board) {
+			component.set('v.board', board)
+			
+			var cellRowList = [];
+			for (var index in board.Cells__r) {
+				var cell = board.Cells__r[index];
+				var row = cell.Y__c - 1;
+				if (!cellRowList[row]) {
+					cellRowList[row] = [];
+				}
+				cellRowList[row].push(cell);
+			}
+			component.set('v.cellRowList', board);
 		}
 	}
 })
