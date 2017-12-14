db5fa5dab8342a4b889acd91ddc845bcc1c53ccb
Brian Kessler <kessler.bm@gmail.com>
2017-12-14 17:53:20 +0100

    Game is playable but there are bugs in the status and victory conditions

--- src/aura/ZV_Cell/ZV_CellController.js
+++ src/aura/ZV_Cell/ZV_CellController.js
@@ -1,5 +1,18 @@
 ({
 	occupy : function(component, event, helper) {
 		helper.occupy(component)
+			.then($A.getCallback(helper.updateBoard(component)));
+	},
+	
+	handleCellUpdateEvent : function(component, event, helper) {
+		var thisCell = component.get('v.cell');
+		var updatedCell = event.getParam('updatedCell');
+		
+		if (
+				thisCell.X__c === updatedCell.X__c
+				&& thisCell.Y__c === updatedCell.Y__c
+			) {
+				component.set('v.cell', updatedCell);
+			}
 	}
 })
--- src/aura/ZV_Cell/ZV_CellHelper.js
+++ src/aura/ZV_Cell/ZV_CellHelper.js
@@ -3,5 +3,39 @@
 		return $LCH.ServerApexPromise(component, 'occupyCell', {
 			cellId: component.get('v.cell.Id')
 		});
+	},
+	
+	updateBoard: function(component) {
+		var self = this;
+		return $A.getCallback(function(cellOccupyResult) {
+			self.updateCells(cellOccupyResult.updatedCellList);
+			self.stopGame(component, cellOccupyResult.gameStatus);
+		});
+	},
+	
+	updateCells: function(updatedCellList) {
+		var self = this;
+		if (updatedCellList) {
+			updatedCellList.forEach(function(updatedCell, index) {
+				var updateCell = self.updateCell(updatedCell);
+				var milliseconds = index === 0 ? 0 : 1000; 
+				$LCH.WaitPromise(milliseconds)
+					.then($A.getCallback(updateCell));
+			});
+		}
+	},
+
+	updateCell: function(updatedCell) {
+		return $A.getCallback(function() {
+			var appEvent = $A.get('e.c:ZV_CellUpdateEvent');
+			appEvent.setParam('updatedCell', updatedCell);
+			appEvent.fire();
+		});
+	},
+
+	stopGame: function(component, gameStatus) {
+		if (gameStatus === 'In Progress') {
+			// TODO
+		}
 	}
 })
